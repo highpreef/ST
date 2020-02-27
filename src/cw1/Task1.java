15136c834aa3b8e477797c53380499918dc09905
highpreef <david.jorge@gmail.com>
2020-02-27 19:33:27 +0000

    Started Task 1

---
+++ src/cw1/Task1.java
@@ -0,0 +1,17 @@
+package cw1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import st1920.automaton.RegExpMatcher;
+
+public class Task1 {
+
+	@Test
+	public void testMatches() {
+		RegExpMatcher.matches("a", "a");
+		fail("Not yet implemented");
+	}
+
+}
